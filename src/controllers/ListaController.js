6d90af7d05200d81e94488fff7ed4d77ea02e1a9
Jean <jeancarlos.na@hotmail.com>
2023-05-23 22:37:23 -0300

    removido login do banco do console.log

--- src/controllers/ListaController.js
+++ src/controllers/ListaController.js
@@ -8,26 +8,26 @@ const responseModel = {
 };
 
 module.exports = {
-    async criaLista(req, res) {
-        const response = {...responseModel}
-        response.data = []
-        response.error = []
-        const { id_user, ds_lista } = req.body;
-        let query = ''
+  async criaLista(req, res) {
+    const response = { ...responseModel };
+    response.data = [];
+    response.error = [];
+    const { id_user, ds_lista } = req.body;
+    let query = "";
 
-        query = ds_lista 
-            ? `INSERT INTO tb_listas (id_usuario, ds_lista) VALUES (${id_user}, '${ds_lista}')` 
-            : `INSERT INTO tb_listas (id_usuario) VALUES (${id_user})`
+    query = ds_lista
+      ? `INSERT INTO tb_listas (id_usuario, ds_lista) VALUES (${id_user}, '${ds_lista}')`
+      : `INSERT INTO tb_listas (id_usuario) VALUES (${id_user})`;
 
-        if ( id_user ) {
-            const [, data] = await connection.query(query)
-            response.success = data.length
-            response.data.push(`Lista cadastrada com sucesso`)
-            response.found = data.length
-        } else {
-            response.error.push('id_user é obrigatorio')
-        }
-
-        return res.json(response)
+    if (id_user) {
+      const [, data] = await connection.query(query);
+      response.success = data.length;
+      response.data.push(`Lista cadastrada com sucesso`);
+      response.found = data.length;
+    } else {
+      response.error.push("id_user é obrigatorio");
     }
-}
+
+    return res.json(response);
+  },
+};
