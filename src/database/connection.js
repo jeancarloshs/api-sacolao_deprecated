b6174681e180c996e8795c9ad8db630054f0bca4
Jean Carlos <jean@naxostelecom.com.br>
2023-05-31 16:05:51 -0300

    alterado connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,34 +1,34 @@
-const Sequelize = require('sequelize');
-const fs = require('fs');
+const Sequelize = require('sequelize')
+const fs = require('fs')
 
-const database = process.env.DATABASE;
-const username = process.env.USER;
-const password = process.env.PASSWORD;
-const host = process.env.HOST;
-const dialect = process.env.DIALECT;
-const options = {};
+const database = process.env.DATABASE
+const username = process.env.USER
+const password = process.env.PASSWORD
+const host = process.env.HOST
+const dialect = process.env.DIALECT
+// const certificad = fs.readFileSync("{./DigiCertGlobalRootCA.crt.pem}")
 
-if (dialect === 'mysql') {
-  options.dialectModule = require('mysql2');
+if (options.dialect === 'mysql') {
+  options.dialectModule = mysql2;
 }
 
 const connection = new Sequelize(database, username, password, {
-  host,
-  dialect,
-  dialectOptions: {
-    requestTimeout: 600000
-  },
-  pool: {
-    min: 0,
-    acquire: 600000,
-    idle: 20000
-  }
-});
+    host,
+    dialect,
+    dialectModule: require('mysql2'),
+    dialectOptions: {
+        options: {
+          requestTimeout: 600000
+        }
+      },
+    pool: {
+        min: 0,
+        acquire: 600000,
+        idle: 20000
+      },
+      // ssl: {
+      //   ca: certificad
+      // }
+})
 
-// Uncomment the following lines if you need to use an SSL certificate
-// const certificate = fs.readFileSync("./DigiCertGlobalRootCA.crt.pem");
-// connection.options.ssl = {
-//   ca: certificate
-// };
-
-module.exports = connection;
+module.exports = connection
