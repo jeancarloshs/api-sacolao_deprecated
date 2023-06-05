204cf388905a7cca4f9a6bde084373ebe319d06a
Jean Carlos <jean@naxostelecom.com.br>
2023-06-04 23:46:12 -0300

    alterado para testar conexao com azure

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,30 +1,30 @@
-const Sequelize = require('sequelize')
-const fs = require('fs')
+const Sequelize = require("sequelize");
+const fs = require("fs");
 
-const database = process.env.DATABASE
-const username = process.env.USER
-const password = process.env.PASSWORD
-const host = process.env.HOST
-const dialect = process.env.DIALECT
-// const certificad = fs.readFileSync("{./DigiCertGlobalRootCA.crt.pem}")
+const database = process.env.DATABASE;
+const username = process.env.USER;
+const password = process.env.PASSWORD;
+const host = process.env.HOST;
+const dialect = process.env.DIALECT;
+const certificad = fs.readFileSync("{./DigiCertGlobalRootCA.crt.pem}")
 
 const connection = new Sequelize(database, username, password, {
-    host,
-    dialect,
-    dialectModule: require('mysql2'),
-    dialectOptions: {
-        options: {
-          requestTimeout: 600000
-        }
-      },
-    pool: {
-        min: 0,
-        acquire: 600000,
-        idle: 20000
-      },
-      // ssl: {
-      //   ca: certificad
-      // }
-})
+  host,
+  dialect,
+  // dialectModule: require("mysql2"),
+  dialectOptions: {
+    options: {
+      requestTimeout: 600000,
+    },
+    ssl: {
+      ca: certificad
+    }
+  },
+  pool: {
+    min: 0,
+    acquire: 600000,
+    idle: 20000,
+  },
+});
 
-module.exports = connection
+module.exports = connection;
