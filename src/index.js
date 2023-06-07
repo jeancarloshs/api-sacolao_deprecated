36d6a85370ffecc0bedc265d5769545d9f6188d8
Jean Carlos <jean@naxostelecom.com.br>
2023-06-07 11:01:58 -0300

    alterado tamanho de autorização do upload

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const express = require('express')
 require('dotenv').config()
 
 const app = express()
-app.use(express.json({limit: '200mb'}))
+app.use(express.json({limit: 200000000}))
 
 
 const port = process.env.PORT || 3030
